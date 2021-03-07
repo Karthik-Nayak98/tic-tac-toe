e6e00fe6a94f261925c682029d532593b6901152
Karthik-Nayak98 <karunayak63@gmail.com>
2021-03-07 23:50:26 +0530

    feat: Add 2 player functionality     * Add checks for game draw and win.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const ROW = 3,
-  COL = 3;
+const [ROW, COL] = [3, 3];
 
 const columns = document.querySelectorAll('.column'),
   winner = document.querySelector('.winner');
@@ -9,64 +8,56 @@ let turnValue = 'O',
 
 for (let i = 0; i < COL; ++i) matrix[i] = ['', '', ''];
 
-console.log(matrix);
+winner.textContent = ' ';
+
+const checkForDraw = function () {
+  for (let i = 0; i < ROW; ++i) {
+    for (let j = 0; j < COL; ++j) if (matrix[i][j] === '') return false;
+  }
+  return true;
+};
 
 const checkForWin = function () {
-  let initial = matrix[ROW - 1][COL - 1];
-
   //Checking the columns
   for (let i = 0; i < COL; ++i) {
     const initial = matrix[i][i];
-    let flag = 1;
-    for (let j = 0; j < ROW; ++j) {
-      if (initial != matrix[j][i] && initial.length == 0) {
-        flag = 0;
+    let flag = true;
+    for (let j = 0; j < ROW; ++j)
+      if (initial != matrix[j][i] || initial.length == 0) {
+        flag = false;
         break;
       }
-    }
-    if (flag == 1) {
-      return true;
-    }
+    if (flag) return true;
   }
 
   //Checking the rows
   for (let i = 0; i < ROW; ++i) {
     const initial = matrix[i][i];
-    let flag = 1;
-    for (let j = 0; j < COL; ++j) {
-      if (initial != matrix[i][j] && initial.length == 0) {
-        flag = 0;
+    let flag = true;
+    for (let j = 0; j < COL; ++j)
+      if (initial != matrix[i][j] || initial.length == 0) {
+        flag = false;
         break;
       }
-    }
-    if (flag == 1) {
-      return true;
-    }
+    if (flag) return true;
   }
 
-  let flag;
+  let initial = matrix[ROW - 1][COL - 1];
   // Checking the diagonal;
-  for (let i = 0; i < ROW; ++i) {
-    flag = 1;
-    for (let j = 0; j < COL; ++j) {
-      if (i == j && matrix[i][j] === initial && initial.length == 0) {
-        flag = 0;
-        break;
-      }
-    }
-    if (flag == 0) break;
-  }
-  if ((flag = 1)) return true;
-
-  // if (initial.length != 0 && matrix[0][0] === matrix[1][1] && matrix[1][1] === matrix[2][2])
-  //     return true;
+  if (
+    initial.length != 0 &&
+    initial === matrix[0][0] &&
+    matrix[0][0] === matrix[1][1] &&
+    matrix[1][1] === matrix[2][2]
+  )
+    return true;
 
   initial = matrix[0][2];
   //Checking the opposite diagonal
   if (
     initial.length != 0 &&
     initial === matrix[0][2] &&
-    matrxix[0][2] === matrix[1][1] &&
+    matrix[0][2] === matrix[1][1] &&
     matrix[1][1] === matrix[2][0]
   )
     return true;
@@ -74,13 +65,15 @@ const checkForWin = function () {
   return false;
 };
 
-// Changes the turn value after each click
+// Changing the turn value after each click
 const changeTurnValue = function () {
   if (turnValue === 'X') turnValue = 'O';
   else turnValue = 'X';
 };
 
-const updateMatrix = function (row, col, turnValue) {
+const updateMatrix = function (value, turnValue) {
+  let row = Math.floor(value / 10);
+  let col = value % 10;
   matrix[row][col] = turnValue;
   console.log(matrix);
 };
@@ -89,13 +82,9 @@ columns.forEach(function (column) {
   column.addEventListener('click', function (e) {
     const value = e.target.dataset.value;
     if (column.textContent.length === 0) column.textContent = turnValue;
-    row = Math.floor(value / 10);
-    col = value % 10;
-    updateMatrix(row, col, turnValue);
-    const bool = checkForWin();
-    console.log(bool);
-    if (bool) winner.textContent = `${turnValue} Wins`;
-
+    updateMatrix(value, turnValue);
+    if (checkForWin()) winner.textContent = `${turnValue} Wins`;
+    if (checkForDraw()) winner.textContent = `Draw`;
     changeTurnValue();
   });
 });
