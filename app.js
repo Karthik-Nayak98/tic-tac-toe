fc5bd7ea5a2d971d6d9ee68e0faf177bf135b76a
Karthik-Nayak98 <karunayak63@gmail.com>
2021-03-16 18:22:38 +0530

    fix: Updated array to one dimensional

--- app.js
+++ app.js
@@ -1,88 +1,60 @@
 const [ROW, COL] = [3, 3];
 
 const columns = document.querySelectorAll('.column'),
-  winner = document.querySelector('.winner');
+      winner = document.querySelector('.winner');
 
 let turnValue = 'O',
-  matrix = new Array(ROW);
+    matrix = new Array(9).fill(null);
 
-for (let i = 0; i < COL; ++i) matrix[i] = ['', '', ''];
+
+
 
 winner.textContent = ' ';
 
 const checkForDraw = function () {
-  for (let i = 0; i < ROW; ++i) {
-    for (let j = 0; j < COL; ++j) if (matrix[i][j] === '') return false;
-  }
+  for (let i = 0; i < 9; ++i)
+   if (matrix[i] === null) return false;
   return true;
 };
 
 const checkForWin = function () {
   //Checking the columns
-  for (let i = 0; i < COL; ++i) {
-    const initial = matrix[i][i];
-    let flag = true;
-    for (let j = 0; j < ROW; ++j)
-      if (initial != matrix[j][i] || initial.length == 0) {
-        flag = false;
-        break;
-      }
-    if (flag) return true;
-  }
+  for(let i = 0; i < 3; i++)
+    if (matrix[i] != null && matrix[i] === matrix[i+3] && matrix[i+3] === matrix[i+6])
+      return true;
 
-  //Checking the rows
-  for (let i = 0; i < ROW; ++i) {
-    const initial = matrix[i][i];
-    let flag = true;
-    for (let j = 0; j < COL; ++j)
-      if (initial != matrix[i][j] || initial.length == 0) {
-        flag = false;
-        break;
-      }
-    if (flag) return true;
-  }
+  // Checking for Rows
+  for (let i = 0; i < 9; i = i+3)
+     if (matrix[i] != null && matrix[i] === matrix[i + 1] && matrix[i + 1] === matrix[i + 2])
+       return true;
 
-  let initial = matrix[ROW - 1][COL - 1];
-  // Checking the diagonal;
-  if (
-    initial.length != 0 &&
-    initial === matrix[0][0] &&
-    matrix[0][0] === matrix[1][1] &&
-    matrix[1][1] === matrix[2][2]
-  )
+  // Checking for diagonals
+  if (matrix[0] != null && matrix[0] === matrix[4] && matrix[4] === matrix[8])
     return true;
 
-  initial = matrix[0][2];
-  //Checking the opposite diagonal
-  if (
-    initial.length != 0 &&
-    initial === matrix[0][2] &&
-    matrix[0][2] === matrix[1][1] &&
-    matrix[1][1] === matrix[2][0]
-  )
-    return true;
+  // Checking for opposite diagonal
+  if (matrix[2] != null && matrix[2] === matrix[4] && matrix[4] === matrix[6])
+     return true;
 
   return false;
 };
 
 // Changing the turn value after each click
 const changeTurnValue = function () {
-  if (turnValue === 'X') turnValue = 'O';
-  else turnValue = 'X';
+  // if (turnValue === 'X') turnValue = 'O';
+  // else turnValue = 'X';
+  turnValue = turnValue === 'X' ? 'O' : 'X';
 };
 
-const updateMatrix = function (value, turnValue) {
-  let row = Math.floor(value / 10);
-  let col = value % 10;
-  matrix[row][col] = turnValue;
+const updateMatrix = function (index, turnValue) {
+  matrix[index] = turnValue;
   console.log(matrix);
 };
 
-columns.forEach(function (column) {
+columns.forEach(function (column, index) {
   column.addEventListener('click', function (e) {
-    const value = e.target.dataset.value;
     if (column.textContent.length === 0) column.textContent = turnValue;
-    updateMatrix(value, turnValue);
+    updateMatrix(index, turnValue);
     if (checkForWin()) winner.textContent = `${turnValue} Wins`;
     if (checkForDraw()) winner.textContent = `Draw`;
     changeTurnValue();
