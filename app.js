8ebc75ea7e9a4a8f515838f219c6d80db54b6eed
Karthik-Nayak98 <karunayak63@gmail.com>
2021-03-22 17:56:53 +0530

    feat: Add minimax algorithm
        * This is brute force algorithm which will find the next optimal
        position.

--- app.js
+++ app.js
@@ -1,62 +1,195 @@
-const [ROW, COL] = [3, 3];
-
-const columns = document.querySelectorAll('.column'),
-      winner = document.querySelector('.winner');
-
-let turnValue = 'O',
-    matrix = new Array(9).fill(null);
-
-
-
-
-winner.textContent = ' ';
-
-const checkForDraw = function () {
-  for (let i = 0; i < 9; ++i)
-   if (matrix[i] === null) return false;
-  return true;
+const infinity = 9999999;
+let score, bestIndex, bestScore, value, height;
+// Setting scores for wins and ties for players
+const scores = {
+  X: -1,
+  O: 1,
+  Tie: 0,
 };
 
-const checkForWin = function () {
+const depth = 9;
+const human = 'X',
+  ai = 'O';
+
+const columns = document.querySelectorAll('.column'),
+  winner = document.querySelector('.winner');
+
+let tictactoe = new Array(9),
+  result = '',
+  turnValue = human;
+
+tictactoe = [null, null, null, null, null, null, null, null, null];
+
+const checkForWin = function (tictactoe) {
   //Checking the columns
-  for(let i = 0; i < 3; i++)
-    if (matrix[i] != null && matrix[i] === matrix[i+3] && matrix[i+3] === matrix[i+6])
+  for (let i = 0; i < 3; i++)
+    if (
+      tictactoe[i] != null &&
+      tictactoe[i] === tictactoe[i + 3] &&
+      tictactoe[i + 3] === tictactoe[i + 6]
+    )
       return true;
 
   // Checking for Rows
-  for (let i = 0; i < 9; i = i+3)
-     if (matrix[i] != null && matrix[i] === matrix[i + 1] && matrix[i + 1] === matrix[i + 2])
-       return true;
+  for (let i = 0; i < 9; i = i + 3)
+    if (
+      tictactoe[i] != null &&
+      tictactoe[i] === tictactoe[i + 1] &&
+      tictactoe[i + 1] === tictactoe[i + 2]
+    )
+      return true;
 
   // Checking for diagonals
-  if (matrix[0] != null && matrix[0] === matrix[4] && matrix[4] === matrix[8])
+  if (
+    tictactoe[0] != null &&
+    tictactoe[0] === tictactoe[4] &&
+    tictactoe[4] === tictactoe[8]
+  )
     return true;
 
   // Checking for opposite diagonal
-  if (matrix[2] != null && matrix[2] === matrix[4] && matrix[4] === matrix[6])
-     return true;
+  if (
+    tictactoe[2] != null &&
+    tictactoe[2] === tictactoe[4] &&
+    tictactoe[4] === tictactoe[6]
+  )
+    return true;
 
   return false;
 };
 
+const minimax = function (tictactoe, isMax, height) {
+  // console.log(tictactoe);
+  let score, bestScore;
+  if (checkForDraw(tictactoe)) return 0;
+  if (isMax) {
+    bestScore = -infinity;
+    for (let i = 0; i < 9; ++i) {
+      if (tictactoe[i] === null) {
+        tictactoe[i] = ai;
+        if (checkForWin(tictactoe)) {
+          score = 10;
+        } else {
+          score = minimax(tictactoe, false, height + 1);
+        }
+        tictactoe[i] = null; // Reverting to the original value
+        bestScore = Math.max(score, bestScore);
+        // bestIndex = index;
+      }
+    }
+    // console.log(bestScore);
+    return bestScore;
+  } else {
+    bestScore = infinity;
+    for (let i = 0; i < 9; ++i) {
+      if (tictactoe[i] === null) {
+        tictactoe[i] = human;
+        if (checkForWin(tictactoe)) {
+          score = -10;
+          // console.log(`max ${score}`);
+        } else {
+          score = minimax(tictactoe, true, height + 1);
+        }
+        // Reverting to the original value
+        bestScore = Math.min(score, bestScore);
+        tictactoe[i] = null;
+      }
+    }
+    return bestScore;
+  }
+};
+
+winner.textContent = '';
+
+const checkForDraw = function (tictactoe) {
+  for (let i = 0; i < 9; ++i) if (tictactoe[i] === null) return false;
+  return true;
+};
+
 // Changing the turn value after each click
 const changeTurnValue = function () {
-  // if (turnValue === 'X') turnValue = 'O';
-  // else turnValue = 'X';
-  turnValue = turnValue === 'X' ? 'O' : 'X';
+  turnValue = turnValue === human ? ai : human;
 };
 
-const updateMatrix = function (index, turnValue) {
-  matrix[index] = turnValue;
-  console.log(matrix);
+const updateArray = function (index, turnValue) {
+  // console.log(index);
+  if (tictactoe[index] === null) tictactoe[index] = turnValue;
+  for (let i = 0; i < 9; ++i) {
+    columns[i].innerHTML = tictactoe[i];
+  }
 };
 
-columns.forEach(function (column, index) {
-  column.addEventListener('click', function (e) {
-    if (column.textContent.length === 0) column.textContent = turnValue;
-    updateMatrix(index, turnValue);
-    if (checkForWin()) winner.textContent = `${turnValue} Wins`;
-    if (checkForDraw()) winner.textContent = `Draw`;
-    changeTurnValue();
+const checkFilledCells = function () {
+  let count = 0;
+  for (let i = 0; i < 9; ++i) if (tictactoe[i] != null) count++;
+  return count;
+};
+
+const displayResult = function (turnValue) {
+  if (checkForWin(tictactoe)) {
+    winner.textContent = `${turnValue} Wins`;
+    return;
+  }
+  if (checkForDraw(tictactoe)) {
+    winner.textContent = `Game is a Tie`;
+    return;
+  }
+};
+
+const beginGame = function () {
+  columns.forEach(function (column, index) {
+    column.addEventListener('click', function (e) {
+      updateArray(index, human);
+      // displayResult(turnValue);
+      if (checkForWin(tictactoe)) {
+        winner.textContent = `X Wins`;
+        return;
+      }
+      if (checkForDraw(tictactoe)) {
+        winner.textContent = `Game is a Tie`;
+        return;
+      }
+      // changeTurnValue();
+
+      let bestScore = -infinity;
+      let bestIndex = -1;
+      let score;
+      for (let i = 0; i < 9; ++i) {
+        // let height = checkFilledCells();
+        console.log(`height ${height}`);
+        if (tictactoe[i] === null) {
+          tictactoe[i] = ai;
+          score = minimax(tictactoe, false, 0);
+          tictactoe[i] = null;
+          console.log('score-value ' + score);
+        }
+        if (score > bestScore) {
+          bestScore = score;
+          bestIndex = i;
+        }
+        console.log(`bestindex ${bestIndex}`);
+      }
+      updateArray(bestIndex, ai);
+      checkForWin(tictactoe);
+      if (checkForWin(tictactoe)) {
+        winner.textContent = `O Wins`;
+        return;
+      }
+      if (checkForDraw(tictactoe)) {
+        winner.textContent = `Game is a Tie`;
+        return;
+      }
+      // displayResult(turnValue);
+      // changeTurnValue();
+      console.log(tictactoe);
+    });
   });
+};
+
+// console.log(tictactoe);
+updateArray();
+
+result = beginGame();
+columns.forEach(function (column, index) {
+  column.removeEventListener('click', function () {});
 });
