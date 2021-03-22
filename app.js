5d37287c486e12cd9247bd4f7b20738efe65001a
Karthik-Nayak98 <karunayak63@gmail.com>
2021-03-22 19:09:22 +0530

    fix: Making algorithm little more smarter
        * Subracting the return value of minimax function with the height of
        the tree.

--- app.js
+++ app.js
@@ -1,192 +1,146 @@
-const infinity = 9999999;
-let score, bestIndex, bestScore, value, height;
-// Setting scores for wins and ties for players
-const scores = {
-  X: -1,
-  O: 1,
-  Tie: 0,
-};
-
-const depth = 9;
-const human = 'X',
-  ai = 'O';
+const INFINTIY = 9999999,
+  HUMAN = 'X',
+  COMPUTER = 'O';
 
 const columns = document.querySelectorAll('.column'),
   winner = document.querySelector('.winner');
 
-let tictactoe = new Array(9),
-  result = '',
-  turnValue = human;
+// Setting scores for wins and ties for players
+const scores = {
+  X: -10,
+  O: 10,
+};
 
-tictactoe = [null, null, null, null, null, null, null, null, null];
+let board = new Array(9).fill(null),
+  turnValue = HUMAN;
 
-const checkForWin = function (tictactoe) {
+const checkForWin = function (board) {
   //Checking the columns
   for (let i = 0; i < 3; i++)
     if (
-      tictactoe[i] != null &&
-      tictactoe[i] === tictactoe[i + 3] &&
-      tictactoe[i + 3] === tictactoe[i + 6]
+      board[i] != null &&
+      board[i] === board[i + 3] &&
+      board[i] === board[i + 6]
     )
       return true;
 
   // Checking for Rows
   for (let i = 0; i < 9; i = i + 3)
     if (
-      tictactoe[i] != null &&
-      tictactoe[i] === tictactoe[i + 1] &&
-      tictactoe[i + 1] === tictactoe[i + 2]
+      board[i] != null &&
+      board[i] === board[i + 1] &&
+      board[i] === board[i + 2]
     )
       return true;
 
   // Checking for diagonals
-  if (
-    tictactoe[0] != null &&
-    tictactoe[0] === tictactoe[4] &&
-    tictactoe[4] === tictactoe[8]
-  )
+  if (board[0] != null && board[0] === board[4] && board[4] === board[8])
     return true;
 
   // Checking for opposite diagonal
-  if (
-    tictactoe[2] != null &&
-    tictactoe[2] === tictactoe[4] &&
-    tictactoe[4] === tictactoe[6]
-  )
+  if (board[2] != null && board[2] === board[4] && board[4] === board[6])
     return true;
-
   return false;
 };
 
-const minimax = function (tictactoe, isMax, height) {
-  // console.log(tictactoe);
+const minimax = function (board, isMax, height) {
   let score, bestScore;
-  if (checkForDraw(tictactoe)) return 0;
+
+  if (checkForWin(board)) return scores[turnValue];
+  if (checkForDraw(board)) return 0;
+
   if (isMax) {
-    bestScore = -infinity;
-    for (let i = 0; i < 9; ++i) {
-      if (tictactoe[i] === null) {
-        tictactoe[i] = ai;
-        if (checkForWin(tictactoe)) {
-          score = 10;
+    bestScore = -INFINTIY;
+    for (let i = 0; i < 9; ++i)
+      if (board[i] === null) {
+        board[i] = COMPUTER;
+        if (checkForWin(board)) {
+          score = 10 - height;
         } else {
-          score = minimax(tictactoe, false, height + 1);
+          score = minimax(board, false, height + 1);
         }
-        tictactoe[i] = null; // Reverting to the original value
+        board[i] = null; // Reverting to the original value
         bestScore = Math.max(score, bestScore);
-        // bestIndex = index;
       }
-    }
-    // console.log(bestScore);
-    return bestScore;
   } else {
-    bestScore = infinity;
-    for (let i = 0; i < 9; ++i) {
-      if (tictactoe[i] === null) {
-        tictactoe[i] = human;
-        if (checkForWin(tictactoe)) {
-          score = -10;
-          // console.log(`max ${score}`);
+    bestScore = INFINTIY;
+    for (let i = 0; i < 9; ++i)
+      if (board[i] === null) {
+        board[i] = HUMAN;
+        if (checkForWin(board)) {
+          score = -10 + height;
         } else {
-          score = minimax(tictactoe, true, height + 1);
+          score = minimax(board, true, height + 1);
         }
         // Reverting to the original value
         bestScore = Math.min(score, bestScore);
-        tictactoe[i] = null;
+        board[i] = null;
       }
-    }
-    return bestScore;
   }
+  return bestScore;
 };
 
 winner.textContent = '';
 
-const checkForDraw = function (tictactoe) {
-  for (let i = 0; i < 9; ++i) if (tictactoe[i] === null) return false;
+const checkForDraw = function (board) {
+  for (let i = 0; i < 9; ++i) if (board[i] === null) return false;
   return true;
 };
 
 // Changing the turn value after each click
 const changeTurnValue = function () {
-  turnValue = turnValue === human ? ai : human;
+  turnValue = turnValue === HUMAN ? COMPUTER : HUMAN;
 };
 
 const updateArray = function (index, turnValue) {
-  // console.log(index);
-  if (tictactoe[index] === null) tictactoe[index] = turnValue;
+  if (board[index] === null) board[index] = turnValue;
   for (let i = 0; i < 9; ++i) {
-    columns[i].innerHTML = tictactoe[i];
+    columns[i].innerHTML = board[i];
   }
-};
-
-const checkFilledCells = function () {
-  let count = 0;
-  for (let i = 0; i < 9; ++i) if (tictactoe[i] != null) count++;
-  return count;
 };
 
 const displayResult = function (turnValue) {
-  if (checkForWin(tictactoe)) {
+  if (checkForWin(board)) {
     winner.textContent = `${turnValue} Wins`;
     return;
   }
-  if (checkForDraw(tictactoe)) {
+  if (checkForDraw(board)) {
     winner.textContent = `Game is a Tie`;
     return;
   }
+  changeTurnValue();
 };
 
 const beginGame = function () {
   columns.forEach(function (column, index) {
     column.addEventListener('click', function (e) {
-      updateArray(index, human);
-      // displayResult(turnValue);
-      if (checkForWin(tictactoe)) {
-        winner.textContent = `X Wins`;
-        return;
-      }
-      if (checkForDraw(tictactoe)) {
-        winner.textContent = `Game is a Tie`;
-        return;
-      }
+      updateArray(index, HUMAN);
+      displayResult(turnValue);
       // changeTurnValue();
 
-      let bestScore = -infinity;
+      let bestScore = -INFINTIY;
       let bestIndex = -1;
       let score;
       for (let i = 0; i < 9; ++i) {
-        // let height = checkFilledCells();
-        console.log(`height ${height}`);
-        if (tictactoe[i] === null) {
-          tictactoe[i] = ai;
-          score = minimax(tictactoe, false, 0);
-          tictactoe[i] = null;
-          console.log('score-value ' + score);
+        // console.log(`height ${height}`);
+        if (board[i] === null) {
+          board[i] = COMPUTER;
+          score = minimax(board, false, 0);
+          board[i] = null;
         }
         if (score > bestScore) {
           bestScore = score;
           bestIndex = i;
         }
-        console.log(`bestindex ${bestIndex}`);
       }
-      updateArray(bestIndex, ai);
-      checkForWin(tictactoe);
-      if (checkForWin(tictactoe)) {
-        winner.textContent = `O Wins`;
-        return;
-      }
-      if (checkForDraw(tictactoe)) {
-        winner.textContent = `Game is a Tie`;
-        return;
-      }
-      // displayResult(turnValue);
-      // changeTurnValue();
-      console.log(tictactoe);
+      updateArray(bestIndex, COMPUTER);
+      checkForWin(board);
+      displayResult(turnValue);
     });
   });
 };
 
-// console.log(tictactoe);
+// console.log(board);
 updateArray();
 
 result = beginGame();
